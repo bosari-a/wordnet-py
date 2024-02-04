d95e43d3db613d9d746789bf29fd133bfd5b5579
bosari-a <ahmedelbosarimc@gmail.com>
2024-02-05 02:07:09 +0400

    completed single word search api

--- main.py
+++ main.py
@@ -22,12 +22,29 @@ DEFAULT_PATH = "./dict"
 '''This is the default path to the wordnet database.'''
 
 
-def search_word_in_index(word: str, ext: str, dict_path: str = DEFAULT_PATH) -> dict | None:
+def get_word_meaning(word: dict, dict_path: str = DEFAULT_PATH):
+    '''This function mutates the `word` dict by
+    adding `definitions` key whose value is different
+    definitions of the word (if found).'''
+    data_path = os.path.join(
+        dict_path, f"data.{TYPES_EXTS[word['word_type']]}")
+    with open(data_path, "r", encoding="utf-8") as fd:
+        buffer_offsets = word.get("buffer_offsets", [])
+        for offset, num in zip(buffer_offsets, range(1, len(buffer_offsets)+1)):
+            fd.seek(offset, 0)
+            definition = "".join(fd.readline().split("|")[-1]).strip()
+            word["definitions"] = word.get(
+                "definitions", [])+[f"({TYPES_EXTS[word['word_type']]} {num}) {definition}"]
+
+
+def search_word_in_file(word: str, ext: str, dict_path: str = DEFAULT_PATH) -> dict | None:
     '''This functions searches for `word` provided in the arguments
     in the index.`ext` file where `ext` is also an argument.\n
     If a word is found the it returns a `dict` with the relevant information:
-    `{"word" : word, "buffer_offsets" : list[int], "word_type" : str}`.\n
-         Optional argument `dict_path` defaults to `./dict`.
+    `{"word" : word, "buffer_offsets" : list[int], "word_type" : str, "definitions" : list[str]}`.\n
+    Optional argument `dict_path` defaults to `./dict`.\n
+    This function also calls `get_word_meaning` which adds definitions to
+    the `word["definitions"]` list if a word was found successfully.
     '''
     # 1. create path from extension and dict path
     index_file = os.path.join(dict_path, f"index.{ext}")
@@ -50,29 +67,23 @@ def search_word_in_index(word: str, ext: str, dict_path: str = DEFAULT_PATH) -> 
                 "buffer_offsets": [int(off) for off in BUFFER_OFF.findall(line)],
                 "word_type": word_type
             }
+            get_word_meaning(found, dict_path)
     return found
 
 
-def get_word_meaning(word: dict, dict_path: str = DEFAULT_PATH):
-    '''This function mutates the `word` dict by
-    adding `definitions` key whose value is different
-    definitions of the word (if found).'''
-    data_path = os.path.join(
-        dict_path, f"data.{TYPES_EXTS[word['word_type']]}")
-    with open(data_path, "r", encoding="utf-8") as fd:
-        buffer_offsets = word.get("buffer_offsets", [])
-        for offset, num in zip(buffer_offsets, range(1, len(buffer_offsets)+1)):
-            fd.seek(offset, 0)
-            definition = "".join(fd.readline().split("|")[-1]).strip()
-            word["definitions"] = word.get(
-                "definitions", [])+[f"({TYPES_EXTS[word['word_type']]} {num}) {definition}"]
-
-
-def get_all_meanings(word: str, dict_path: str = DEFAULT_PATH):
+def search_word_in_all(word: str, dict_path: str = DEFAULT_PATH) -> list[str]:
     '''
     This function takes in a word string and sets up multiple threads which
-    call `search_word_in_index` on each wordnet index file and uses
-    the resulting `word_type` and `buffer_offsets` from each thread to
-    parse the data files and get the word definitions for each word type.
+    call `search_word_in_file` on each wordnet index file.\n
+    It returns a `list[str]` which has all the definitions.
     '''
-    return
+    definitions = []
+    with ThreadPoolExecutor() as tp_executor:
+        for ext in TYPES_EXTS.values():
+            future = tp_executor.submit(
+                search_word_in_file, word, ext, dict_path)
+            res = future.result()
+            if res:
+                definitions += res.get("definitions", [])
+    return definitions
+
